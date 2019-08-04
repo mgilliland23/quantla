0a18073b3f2e872794396ed71e88740e02357e3d
Matt Gilliland <mgilliland23@gmail.com>
2019-08-03 22:04:20 -0400

    adjusted objects returned by each function and how the results are being stored in firebase

--- quantla.js
+++ quantla.js
@@ -1,6 +1,7 @@
 var inquirer = require("inquirer");
 // Required for side-effects
 require("firebase/firestore");
+var firebase = require("firebase");
 var Fundamentals = require("./app/checkFundamentals");
 var Prices = require("./app/checkPrices");
 var News = require("./app/checkNews");
@@ -21,84 +22,60 @@ admin.initializeApp({
 
 let db = admin.firestore();
 
-inquirer
-  .prompt([
-    {
-      type: "list",
-      name: "selection",
-      message: "What do you want to do?",
-      choices: ["Check News", "Check Prices", "Check Fundamentals"]
+// Run the analysis for all BTC data sets every 30,000ms (5min)
+// setInterval(runAnalysis(), 30000);
+
+runAnalysis();
+
+function getDateTime() {
+  var currentdate = new Date();
+  var datetime =
+    currentdate.getDate() +
+    "-" +
+    (currentdate.getMonth() + 1) +
+    "-" +
+    currentdate.getFullYear() +
+    " " +
+    currentdate.getHours() +
+    ":" +
+    currentdate.getMinutes() +
+    ":" +
+    currentdate.getSeconds();
+
+  return datetime.toString();
+}
+
+function runAnalysis() {
+  //This will be the date time used to link the different data sets
+  var datetime = getDateTime();
+
+  console.log("Analysis being run at: ", datetime);
+
+  //Run analysis on BTC news. Store results to firestore DB
+  var news = new News();
+  news.checkNews.then(
+    function(result) {
+      db.collection("news")
+        .doc(datetime)
+        .set(result);
     },
-    {
-      when: function(response) {
-        if (response.selection == "Check Fundamentals") return true;
-      },
-      type: "list",
-      name: "runPrice",
-      message: "What key market fundamentals do you want to check?",
-      choices: [
-        "US Sector Performance (realtime)",
-        "Blockchain data",
-        "Mining data",
-        "Pool data - who owns bitcoin!"
-      ]
+    function(err) {
+      console.log(err); // Error: "It broke"
     }
-  ])
-  .then(answers => {
-    switch (answers.selection) {
-      case "Check News":
-        var news = new News();
+  );
 
-        news.checkNews.then(
-          function(result) {
-            // "Stuff worked!", now process each article obj
-            result.forEach(function(newsData) {
-              db.collection("news")
-                .doc(newsData.date)
-                .set(newsData);
-              console.log("Article Data has been added to the database");
-            });
-          },
-          function(err) {
-            console.log(err); // Error: "It broke"
-          }
-        );
-
-        break;
-
-      case "Check Prices":
-        var prices = new Prices();
-        prices.checkPrices.then(function(result) {
-          console.log(result);
-
-          var today = new Date();
-          var date =
-            today.getFullYear() +
-            "-" +
-            (today.getMonth() + 1) +
-            "-" +
-            today.getDate();
-          var mins;
-          if (today.getMinutes() < 10) mins = "0" + today.getMinutes();
-          else mins = today.getMinutes();
-
-          var time = today.getHours() + ":" + mins + ":" + today.getSeconds();
-          var dateTime = date + " " + time;
-
-          db.collection("prices")
-            .doc(dateTime)
-            .set(result);
-          console.log("Prices Data has been added to the database");
-        });
-        break;
-
-      case "Check Fundamentals":
-        var fundamentals = new Fundamentals();
-        fundamentals.checkFundamentals(answers.runPrice);
-        break;
-
-      default:
-        text = "I don't even know how you got here! That is definetly a bug...";
+  //Run analysis on BTC price data. Store results to firestore DB
+  var prices = new Prices();
+  prices.checkPrices.then(
+    function(result) {
+      console.log("check prices results: ", result);
+      db.collection("prices")
+        .doc(datetime)
+        .set(result);
+      console.log("Prices Data has been added to the database: ", datetime);
+    },
+    function(err) {
+      console.log(err); // Error: "It broke"
     }
-  });
-//
+  );
+}
