be2855f09793171ca718646c119768087a819b04
Matt Gilliland <mgilliland23@gmail.com>
2019-08-10 15:00:48 -0400

    changed the way the model is trained

--- public/assets/js/core.js
+++ public/assets/js/core.js
@@ -1,6 +1,6 @@
 var userID;
 //Get the user id of the current user
-firebase.auth().onAuthStateChanged(function (user) {
+firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
     userID = user.uid;
     console.log("User id: " + userID);
@@ -14,55 +14,89 @@ firebase.auth().onAuthStateChanged(function (user) {
 
 grabproposedtradedata();
 
-setInterval(function () {
+setInterval(function() {
   grabproposedtradedata();
   console.log("test");
 }, 30000);
 
 function grabproposedtradedata() {
+  $.getJSON(
+    "https://poloniex.com/public?command=returnOrderBook&currencyPair=USDC_BTC&depth=1",
+    function(results) {
+      // console.log(results.asks[0][0]);
 
-  $.getJSON("https://poloniex.com/public?command=returnOrderBook&currencyPair=USDC_BTC&depth=1", function (results) {
-    // console.log(results.asks[0][0]);
-
-    $('#curr_price').text(
-      (Math.round(results.asks[0][0] * 100) / 100).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')
+      $("#curr_price").text(
+        (Math.round(results.asks[0][0] * 100) / 100)
+          .toFixed(2)
+          .replace(/\d(?=(\d{3})+\.)/g, "$&,")
+      );
+    }
+  );
+  var currDatetime = { time: Math.floor(new Date() / 1000) };
+  $.post("api/decisions", currDatetime, function(data) {
+    console.log("front end decisions: ", data);
+    $("#bif_price").text(
+      (Math.round(data[data.length - 1].buyIfPrice * 100) / 100)
+        .toFixed(2)
+        .replace(/\d(?=(\d{3})+\.)/g, "$&,")
+    );
+    $("#sif_price").text(
+      (Math.round(data[data.length - 1].sellIfPrice * 100) / 100)
+        .toFixed(2)
+        .replace(/\d(?=(\d{3})+\.)/g, "$&,")
     );
 
-  })
+    $("#indication").text(data[data.length - 1].aiDecision);
 
-  $.getJSON("./assets/AIDecision.json", function (fileData) {
-    // console.log(fileData[fileData.length - 1]);
-    // console.log(fileData[fileData.length - 1].CurrentPrice);
-    // console.log(fileData[fileData.length - 1].AIDecision);
-
-    // $('#curr_price').text(
-    //   (Math.round(fileData[fileData.length - 1].CurrentPrice * 100) / 100).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')
-    // );
-
-    $('#bif_price').text(
-      (Math.round(fileData[fileData.length - 1].BuyIfPrice * 100) / 100).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')
-    );
-    $('#sif_price').text(
-      (Math.round(fileData[fileData.length - 1].SellIfPrice * 100) / 100).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')
-    );
-
-    $('#indication').text(fileData[fileData.length - 1].AIDecision);
-
-    if (fileData[fileData.length - 1].AIDecision == "Buy") {
-      $("#indication_img").attr('class', 'fas fa-arrow-circle-up');
+    if (data[data.length - 1].aiDecision == "Buy") {
+      $("#indication_img").attr("class", "fas fa-arrow-circle-up");
       $("#indication_img").css("color", "greenyellow");
       $("#indication").css("color", "greenyellow");
-    }
-    else if (fileData[fileData.length - 1].AIDecision == "Sell") {
-      $("#indication_img").attr('class', 'fas fa-arrow-circle-down');
+    } else if (data[data.length - 1].aiDecision == "Sell") {
+      $("#indication_img").attr("class", "fas fa-arrow-circle-down");
       $("#indication_img").css("color", "red");
       $("#indication").css("color", "red");
-    }
-    else {
-      $("#indication_img").attr('class', 'fas fa-exchange-alt');
+    } else {
+      $("#indication_img").attr("class", "fas fa-exchange-alt");
       $("#indication_img").css("color", "yellow");
       $("#indication").css("color", "yellow");
     }
-
   });
+
+  // $.getJSON("./assets/AIDecision.json", function(data) {
+  //   // console.log(fileData[fileData.length - 1]);
+  //   // console.log(fileData[fileData.length - 1].CurrentPrice);
+  //   // console.log(fileData[fileData.length - 1].AIDecision);
+
+  //   // $('#curr_price').text(
+  //   //   (Math.round(fileData[fileData.length - 1].CurrentPrice * 100) / 100).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')
+  //   // );
+
+  //   $("#bif_price").text(
+  //     (Math.round(fileData[fileData.length - 1].BuyIfPrice * 100) / 100)
+  //       .toFixed(2)
+  //       .replace(/\d(?=(\d{3})+\.)/g, "$&,")
+  //   );
+  //   $("#sif_price").text(
+  //     (Math.round(fileData[fileData.length - 1].SellIfPrice * 100) / 100)
+  //       .toFixed(2)
+  //       .replace(/\d(?=(\d{3})+\.)/g, "$&,")
+  //   );
+
+  //   $("#indication").text(fileData[fileData.length - 1].AIDecision);
+
+  //   if (fileData[fileData.length - 1].AIDecision == "Buy") {
+  //     $("#indication_img").attr("class", "fas fa-arrow-circle-up");
+  //     $("#indication_img").css("color", "greenyellow");
+  //     $("#indication").css("color", "greenyellow");
+  //   } else if (fileData[fileData.length - 1].AIDecision == "Sell") {
+  //     $("#indication_img").attr("class", "fas fa-arrow-circle-down");
+  //     $("#indication_img").css("color", "red");
+  //     $("#indication").css("color", "red");
+  //   } else {
+  //     $("#indication_img").attr("class", "fas fa-exchange-alt");
+  //     $("#indication_img").css("color", "yellow");
+  //     $("#indication").css("color", "yellow");
+  //   }
+  // });
 }
