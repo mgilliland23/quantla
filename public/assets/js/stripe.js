1b76f8c4d960d526f9deff031990578f06898530
Matt Gilliland <mgilliland23@gmail.com>
2019-08-11 19:28:52 -0400

    debugged success and cancelled pages

--- public/assets/js/stripe.js
+++ public/assets/js/stripe.js
@@ -1,29 +1,33 @@
-var stripe = Stripe("pk_live_G54aTnWHKfG5ii4Kj34vUerI004h2VSbzI");
+$(document).on("load", function() {
+  var stripe = Stripe("pk_live_G54aTnWHKfG5ii4Kj34vUerI004h2VSbzI");
 
-var checkoutButton = document.getElementById(
-  "checkout-button-sku_FbcRETN9HdQ97p"
-);
-checkoutButton.addEventListener("click", function() {
-  // When the customer clicks on the button, redirect
-  // them to Checkout.
-  stripe
-    .redirectToCheckout({
-      items: [{ sku: "sku_FbcRETN9HdQ97p", quantity: 1 }],
+  var checkoutButton = document.getElementById(
+    "checkout-button-sku_FbcRETN9HdQ97p"
+  );
+  checkoutButton.addEventListener("click", function() {
+    // When the customer clicks on the button, redirect
+    // them to Checkout.
+    stripe
+      .redirectToCheckout({
+        items: [{ sku: "sku_FbcRETN9HdQ97p", quantity: 1 }],
 
-      // Do not rely on the redirect to the successUrl for fulfilling
-      // purchases, customers may not always reach the success_url after
-      // a successful payment.
-      // Instead use one of the strategies described in
-      // https://stripe.com/docs/payments/checkout/fulfillment
-      successUrl: window.location.protocol + "//www.quantla.com/success",
-      cancelUrl: window.location.protocol + "//www.quantla.com/canceled"
-    })
-    .then(function(result) {
-      if (result.error) {
-        // If `redirectToCheckout` fails due to a browser or network
-        // error, display the localized error message to your customer.
-        var displayError = document.getElementById("error-message");
-        displayError.textContent = result.error.message;
-      }
-    });
+        // Do not rely on the redirect to the successUrl for fulfilling
+        // purchases, customers may not always reach the success_url after
+        // a successful payment.
+        // Instead use one of the strategies described in
+        // https://stripe.com/docs/payments/checkout/fulfillment
+        successUrl:
+          window.location.protocol + "//www.quantla.herokuapp.com/success.html",
+        cancelUrl:
+          window.location.protocol + "//www.quantla.herokuapp.com/canceled"
+      })
+      .then(function(result) {
+        if (result.error) {
+          // If `redirectToCheckout` fails due to a browser or network
+          // error, display the localized error message to your customer.
+          var displayError = document.getElementById("error-message");
+          displayError.textContent = result.error.message;
+        }
+      });
+  });
 });
