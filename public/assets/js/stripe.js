241fb8c1afb1b33c9733060d761d1ca22e960b0f
Matt Gilliland <mgilliland23@gmail.com>
2019-08-11 18:16:35 -0400

    added product

--- public/assets/js/stripe.js
+++ public/assets/js/stripe.js
@@ -1,22 +1,22 @@
 var stripe = Stripe("pk_live_G54aTnWHKfG5ii4Kj34vUerI004h2VSbzI");
 
 var checkoutButton = document.getElementById(
-  "checkout-button-sku_FbIKf30wocKTqr"
+  "checkout-button-sku_FbcRETN9HdQ97p"
 );
 checkoutButton.addEventListener("click", function() {
   // When the customer clicks on the button, redirect
   // them to Checkout.
   stripe
     .redirectToCheckout({
-      items: [{ sku: "sku_FbIKf30wocKTqr", quantity: 1 }],
+      items: [{ sku: "sku_FbcRETN9HdQ97p", quantity: 1 }],
 
       // Do not rely on the redirect to the successUrl for fulfilling
       // purchases, customers may not always reach the success_url after
       // a successful payment.
       // Instead use one of the strategies described in
       // https://stripe.com/docs/payments/checkout/fulfillment
-      successUrl: window.location.protocol + "//www.quantla.com/core",
-      cancelUrl: window.location.protocol + "//www.quantla.com/"
+      successUrl: window.location.protocol + "//www.quantla.com/success",
+      cancelUrl: window.location.protocol + "//www.quantla.com/canceled"
     })
     .then(function(result) {
       if (result.error) {
